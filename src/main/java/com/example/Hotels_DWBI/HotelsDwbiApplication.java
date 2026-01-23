dc23fdac5a166ec43c81a327b7a5a0ab8e68bbdc
Cosmina Gheorghe <cosminagheorghe47@gmail.com>
2026-01-23 18:18:57 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/example/Hotels_DWBI/HotelsDwbiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hotels_DWBI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsDwbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsDwbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hotels_DWBI/HotelsDwbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hotels_DWBI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsDwbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
